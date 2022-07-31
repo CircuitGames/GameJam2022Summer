a6572ccd0faa3cb3b43a7dc775df263a6cf48774
Swiftfire <swiftfireseven@gmail.com>
2022-07-31 10:10:19 -0500

    modify scroll

--- scripts/Background_scripts/Scroll.cs
+++ scripts/Background_scripts/Scroll.cs
@@ -5,6 +5,22 @@ public class Scroll: KinematicBody2D
     [Export]
     float scrollSpeed = 50;
 
+    Area2D self;
+
+
+    public override void _Ready()
+    {
+        self = ((Area2D)GetNode("Area2D"));
+        self.Connect("area_entered", this, "OnKillboxBodyEntered");
+        self.Connect("body_entered", this, "OnKillboxBodyEntered");
+
+    }
+
+    public void OnKillboxBodyEntered(PhysicsBody2D body)
+    {
+        QueueFree();
+    }
+
 
 
     public override void _PhysicsProcess(float delta)
