447deaf344c81705945c7d5c690a745e1cbdd30e
Swiftfire <swiftfireseven@gmail.com>
2022-08-01 07:18:49 -0500

    imported art
    set up art assets in the scene and prepped the rolling background
    
    this is making the scene look pretty and groundwork for the background
    reset script, also made backgrounds children of a spawner for
    future use in the script

--- scripts/Background_scripts/Scroll.cs
+++ scripts/Background_scripts/Scroll.cs
@@ -1,18 +1,14 @@
 using Godot;
 
-public class Scroll: KinematicBody2D
+public class Scroll: Area2D
 {
     [Export]
     float scrollSpeed = 50;
 
-    Area2D self;
-
-
     public override void _Ready()
     {
-        self = ((Area2D)GetNode("Area2D"));
-        self.Connect("area_entered", this, "OnKillboxBodyEntered");
-        self.Connect("body_entered", this, "OnKillboxBodyEntered");
+        Connect("area_entered", this, "OnKillboxBodyEntered");
+        Connect("body_entered", this, "OnKillboxBodyEntered");
 
     }
 
@@ -25,11 +21,9 @@ public class Scroll: KinematicBody2D
 
     public override void _PhysicsProcess(float delta)
     {
-        Vector2 location = Vector2.Zero;
-        location.x = 0;
-        location.y = 1;
+        Vector2 location = Vector2.Down;
 
-        MoveAndCollide(location * scrollSpeed * delta);
+        Position += (location * scrollSpeed * delta);
     }
 
 }
