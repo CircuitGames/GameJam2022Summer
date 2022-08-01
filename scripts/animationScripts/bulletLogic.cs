eb390676ceec1127be43a930509a6b544c8c3057
Alexander Evans <alexander.evans.tech@protonmail.com>
2022-07-31 20:19:37 -0500

    Added Proper Scene changing

--- scripts/animationScripts/bulletLogic.cs
+++ scripts/animationScripts/bulletLogic.cs
@@ -7,26 +7,29 @@ public class bulletLogic : Node2D
     float speed = 1000;
     private AnimatedSprite sprite;
 
-    Area2D self;
+    Area2D area2DNode;
 
     public bool reloaded = true;
 
     Timer cooldown;
 
 
-    public override void _Ready ()
+    public override void _Ready()
     {
         initiateAnimations();
         reloaded = false;
-        self = ( ( Area2D ) GetNode( "Area2D" ) );
-        cooldown = ( ( Timer ) GetNode( "Timer" ) );
-        self.Connect( "area_entered", this, "OnBulletBodyEntered" );
-        self.Connect( "body_entered", this, "OnBulletBodyEntered" );
+
+        cooldown = GetNode<Timer>( "Timer" );
         cooldown.Connect( "timeout", this, "onTimerTimeout" );
+
+        area2DNode = GetNode<Area2D>( "Area2D" );
+        area2DNode.Connect( "area_entered", this, "OnBulletBodyEntered" );
+        area2DNode.Connect( "body_entered", this, "OnBulletBodyEntered" );
+
 
     }
 
-    public override void _PhysicsProcess ( float delta )
+    public override void _PhysicsProcess( float delta )
     {
         PhysHelp.constrainDelta( ref delta );
         float moveAmount = speed * delta;
@@ -36,12 +39,12 @@ public class bulletLogic : Node2D
     }
 
     //delete the bullet upon collision with a bounding box or player
-    public void OnBulletBodyEntered ( PhysicsBody2D body )
+    public void OnBulletBodyEntered( PhysicsBody2D body )
     {
         QueueFree();
     }
 
-    public void initiateAnimations ()
+    public void initiateAnimations()
     {
         //finds the child sprite of this node
         sprite = GetNode<AnimatedSprite>( "AnimatedSprite" );
@@ -49,7 +52,7 @@ public class bulletLogic : Node2D
         sprite.Play( "default" );
     }
 
-    public void onTimerTimeout ()
+    public void onTimerTimeout()
     {
         reloaded = true;
     }
