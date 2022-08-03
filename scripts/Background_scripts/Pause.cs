2c2c80cac90608d8763a10ae562a7da0d01736e2
Swiftfire <swiftfireseven@gmail.com>
2022-08-03 11:49:30 -0500

    Implemented Pause Screen
    added "ui-pause" in project settings
    
    bound to shift on kb and start on cr

---
+++ scripts/Background_scripts/Pause.cs
@@ -0,0 +1,37 @@
+using Godot;
+using System;
+
+public class Pause : Control
+{
+    bool isPaused=false;
+
+    public override void _Ready()
+    {
+        this.Visible = false;
+        //continue running while paused, without this the game will softlock
+        this.PauseMode = Node2D.PauseModeEnum.Process;
+    }
+    //pause and show screen
+    public void onPauseButtonPressed()
+    {
+        if (isPaused != true && Input.IsActionJustPressed("ui_pause"))
+        {
+            Input.ActionRelease("ui_pause");
+            this.Visible = true;
+            isPaused = true;
+            SetProcess(true);
+            GetTree().Paused = true;
+        }
+        else if (isPaused != false && Input.IsActionJustPressed("ui_pause"))
+        {
+            GetTree().Paused = false;
+            SetProcess(false);
+            isPaused = false;
+            this.Visible = false;
+        }
+    }
+    public override void _Process(float delta)
+    {
+            onPauseButtonPressed();
+    }
+}
